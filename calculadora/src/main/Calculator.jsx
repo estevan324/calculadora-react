60aa8ae991d79f39344005e07510e74641b562f6
Estevan Hernandes Silva de Oliveira <estevanhernandes1@hotmail.com>
2022-09-19 19:47:46 -0300

    Finalização da implementação da funcionalidade da calculadora

--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -3,17 +3,68 @@ import './Calculator.css'
 
 import Button from "../components/Button"
 import Display from "../components/Display"
+
+const initialState = {
+    displayValue: '0',
+    clearDisplay: false,
+    operation: null,
+    values: [0,0],
+    current: 0
+}
+
 class Calculator extends Component {
+
+    state = { ...initialState }  
+
     clearMemory() {
-        console.log('limpar')
+        this.setState({...initialState})
     }
     
     addDigit(n) {
-        console.log(n)
+        if (n === '.' && this.state.displayValue.includes('.')){
+            return
+        }
+
+        const clearDisplay = this.state.displayValue === '0'
+            || this.state.clearDisplay
+        const currentValue = clearDisplay ? '' : this.state.displayValue
+        const displayValue = currentValue + n
+        this.setState({ displayValue, clearDisplay: false })
+
+        if (n !== '.') {
+            const i = this.state.current
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[i] = newValue
+            this.setState({ values })
+            console.log(values)
+        }
     }
     
     setOperation(operation) {
-        console.log(operation)
+        if (this.state.current === 0){
+            this.setState({ operation, current: 1, clearDisplay: true})
+        } else {
+            const equals = operation === '='
+            const currentOperation = this.state.operation
+
+            const values = [...this.state.values]
+            try {
+                values[0] = eval(`${values[0]} ${currentOperation} ${values[1]}`)
+            } catch(e) {
+                values[0] = this.state.values[0]
+            }
+            
+            values[1] = 0
+
+            this.setState({
+                displayValue: values[0],
+                operation: equals ? null : operation,
+                current: equals ? 0 : 1,
+                clearDisplay: !equals,
+                values
+            })
+        }
     }
 
     render() {
@@ -23,7 +74,7 @@ class Calculator extends Component {
 
         return (
             <div className="calculator">
-                <Display value={100} />
+                <Display value={this.state.displayValue} />
                 <Button label="AC" click={clearMemory} triple/>
                 <Button label="/" click={setOperation} operation/>
                 <Button label="7" click={addDigit}/>
